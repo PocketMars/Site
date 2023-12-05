c9a2f2cd6def24a11473107a1bebf18d6a37f092
HappyPepe <92362959+PocketMars@users.noreply.github.com>
2023-12-05 15:26:52 +0300

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,9 @@
+<?php
+$to = "megaultrapepega@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: govnojui@happypepe.zzz.com.ua" . "\r\n" .
+"CC: megaultrapepega@gmail.com";
+
+mail($to,$subject,$txt,$headers);
+?>
